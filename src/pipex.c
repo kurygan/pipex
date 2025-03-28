3563e95f8a95af01bd559a8f2dc5b3764bfec8e5
kurygan <medhy.kettab06@gmail.com>
2025-03-28 23:03:41 +0100

    improve error handling in pipex: update error messages and exit codes

--- src/pipex.c
+++ src/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tylerlover911 <tylerlover911@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 02:00:02 by tylerlover9       #+#    #+#             */
-/*   Updated: 2025/03/25 19:59:11 by mkettab          ###   ########.fr       */
+/*   Updated: 2025/03/28 23:03:09 by tylerlover9      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ void	exec(char *command, char **env)
 	path = set_path(cmd_split[0], env);
 	if (execve(path, cmd_split, env) == -1)
 	{
-		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putstr_fd("Error: command not found: ", 2);
 		ft_putendl_fd(command, 2);
 		free(path);
 		ft_freetab(cmd_split);
-		exit(0);
+		exit(127);
 	}
 }
 
@@ -35,7 +35,7 @@ void	parent(char **av, int fd_pid[2], char **env)
 
 	fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fd < 0)
-		exit(0);
+		exit(1);
 	dup2(fd, 1);
 	dup2(fd_pid[0], 0);
 	close(fd_pid[1]);
@@ -48,7 +48,11 @@ void	kid(char **av, int fd_pid[2], char **env)
 
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
-		exit(0);
+	{
+		ft_putstr_fd("Error: no such file or directory: ", 2);
+		ft_putendl_fd(av[1], 2);
+		exit(1);
+	}
 	dup2(fd, 0);
 	dup2(fd_pid[1], 1);
 	close(fd_pid[0]);
